e640696855f1c0f9edd9f52904e852df7c3e02a9
Roderick <notmine@rjbradley.local>
2018-03-21 10:17:41 -0500

    Updated Article Model and Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,40 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article = find(params[:id])
+
+    if article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+  end
+
   def new
 
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
+
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
 
   private
   def article_params
